dbb3a4c8c57dc257bdd6225eaeba8ffeb7c09415
SeTSeR <sergey200018@bk.ru>
2016-05-21 23:19:10 +0300

    First working variant

--- src/Main.hs
+++ src/Main.hs
@@ -1,13 +1,16 @@
 module Main where
 
+import Data.Array
+
 import Graphics.Gloss.Interface.Pure.Game
+import Graphics.Gloss.Data.ViewPort
 
 import System.Random
 
-type Level = [Int]
+type Level = Array Int Int
 
 data GameState = GS
-    { level :: [Int]
+    { level :: Level
     , x :: Int
     , y :: Int
     , vx :: Int
@@ -23,17 +26,28 @@ main = do
     startGame gen
 
 createLevel :: RandomGen g => Int -> g -> Level
-createLevel length gen = unfoldList (head $ take 1 $ randomRs (0, 100) gen) (take length $ randomRs (-2, 2) gen)
+createLevel length gen = listArray (1, length) $ construct 1 length gen 0
   where
-    unfoldList :: Int -> [Int] -> [Int]
-    unfoldList value [] = [value]
-    unfoldList value (x:xs) = [value + x] ++ (unfoldList (value + x) xs)
+    construct 1 length gen last = let (next, nextGen) = randomR (levelHeight `div` 4, (levelHeight*3) `div` 4) gen
+                                  in [next] ++ (construct 2 length nextGen next)
+    construct x length gen last | x <= length = let (next, nextGen) = randomR (levelHeight `div` 25, (levelHeight*24) `div` 25) gen
+                                                in if (abs (next - last) <= 2)
+                                                   then
+                                                     [next] ++ (construct (x + 1) length nextGen next)
+                                                    else
+                                                     construct x length nextGen last
+
+--createLevel :: RandomGen g => Int -> g -> Level
+--createLevel length gen = listArray (1, length) $ replicate length 25 
 
 startGame :: StdGen -> IO ()
-startGame gen = play (InWindow "Another game" windowSize (240, 160)) (greyN 0.25) 30 (initState gen) renderer handler updater 
+startGame gen = play (InWindow "Another game" windowSize (0, 0)) (greyN 0.25) fps (initState gen) renderer handler updater 
+
+fps :: Int
+fps = 30
 
 windowSize :: (Int, Int)
-windowSize = both (* (round cellSize)) levelSize
+windowSize = both ((* (round cellSize)) . (+ 1)) levelSize
 
 cellSize :: Float 
 cellSize = 15
@@ -41,21 +55,140 @@ cellSize = 15
 both :: (a -> b) -> (a, a) -> (b, b)
 both f (a, b) = (f a, f b)
 
-initState :: StdGen -> GameState
-initState gen = GS level 0 (1 + (head level)) 4 0
+initState :: StdGen -> GameState 
+initState gen = GS level 0 (1 + (level!1)) 1 0
   where
     level = createLevel levelWidth gen
 
-handler :: Event -> world -> world
-handler _ id = id
 
-updater :: Float -> world -> world
-updater _ id = id
+--updater :: Float -> world -> world
+updater _ gs@GS
+  { level = level
+  , x = x
+  , y = y
+  , vx = vx
+  , vy = vy
+  } | x >= levelWidth = gs
+    | (x + vx + 1) >= levelWidth = if (y + vy) >= level!levelWidth
+                                   then gs
+                                     { level = level
+                                     , x = levelWidth
+                                     , y = y + vy
+                                     , vx = vx
+                                     , vy = vy - 1
+                                     }
+                                   else
+                                     if (y + vy) >= level!(x + 1)
+                                     then gs
+                                       { level = level
+                                       , x = x
+                                       , y = y + vy
+                                       , vx = vx
+                                       , vy = vy - 1
+                                       }
+                                     else gs
+                                       { level = level
+                                       , x = x
+                                       , y = y
+                                       , vx = vx
+                                       , vy = 0
+                                       }
 
-renderer :: world -> Picture
-renderer _ = pictures [ uncurry translate (cellToScreen (x, y))
-                      $ color white $ rectangleWire cellSize cellSize
-                      | x <- [0..levelWidth - 1], y <- [0..levelHeight - 1]]
+updater _ gs@GS
+  { level = level
+  , x = x
+  , y = y
+  , vx = vx
+  , vy = vy
+  } = if (y + vy) >= level!(x + vx + 1)
+      then gs
+        { level = level
+        , x = x + vx
+        , y = y + vy
+        , vx = vx
+        , vy = vy - 1
+        }
+      else
+        if (y + vy) >= level!(x + 1)
+        then gs
+          { level = level
+          , x = x
+          , y = y + vy
+          , vx = vx 
+          , vy = vy - 1
+          }
+        else gs
+          { level = level
+          , x = x
+          , y = y
+          , vx = vx
+          , vy = 0
+          }
+
+--renderer :: world -> Picture
+renderer gs@GS
+  { level = level
+  , x = x
+  , y = y
+  } = let width = cellSize * (fromIntegral levelWidth)
+          height = cellSize * (fromIntegral levelHeight)
+          currx x = cellSize * (fromIntegral x) + cellSize/2
+          curry h = (cellSize * (fromIntegral (level!h)))/2
+          currheight h = cellSize * (fromIntegral (level!h))
+          curry' y = cellSize * (fromIntegral y) + cellSize/2
+      in if x < levelWidth
+         then 
+           applyViewPortToPicture viewPort $ 
+           pictures ([ translate (width/2) (height/2) $ color white $ rectangleSolid width height ] ++
+                     [ translate (currx (h - 1)) (curry h) $ color green $ rectangleSolid cellSize (currheight h) | h <- [1..levelWidth]] ++
+                     [ translate (currx x) (curry' y) $ color blue $ circleSolid (cellSize/2)])
+         else
+           applyViewPortToPicture viewPort $ 
+           pictures ([ translate (width/2) (height/2) $ color white $ rectangleSolid width height ] ++
+                     [ translate (currx (h - 1)) (curry h) $ color green $ rectangleSolid cellSize (currheight h) | h <- [1..levelWidth]] ++
+                     [ translate (currx x) (curry' y) $ color blue $ circleSolid (cellSize/2)] ++
+                     [ translate (width/2) (height/2) $ color black $ text "Victory!" ])
+
+viewPort :: ViewPort
+viewPort = ViewPort (both (negate . (/2)) $ cellToScreen levelSize) 0 1
 
 cellToScreen :: (Int, Int) -> (Float, Float)
 cellToScreen = both ((* cellSize) . fromIntegral)
+
+--handler :: Event -> world -> world
+handler event gs@GS
+  { level = level
+  , x = x
+  , y = y
+  , vx = vx
+  , vy = vy
+  } = case event of
+    EventKey (SpecialKey KeyUp) Down _ _ -> gs
+      { level = level
+      , x = x
+      , y = y
+      , vx = vx
+      , vy = vy + 2
+      }
+    EventKey (SpecialKey KeyRight) Down _ _ -> gs
+      { level = level
+      , x = x
+      , y = y
+      , vx = vx + 2
+      , vy = vy
+      }
+    EventKey (SpecialKey KeyDown) Down _ _ -> gs
+      { level = level
+      , x = x
+      , y = y
+      , vx = vx
+      , vy = vy - 2
+      }
+    EventKey (SpecialKey KeyLeft) Down _ _ -> gs
+      { level = level
+      , x = x
+      , y = y
+      , vx = vx - 2
+      , vy = vy
+      }
+    otherwise -> gs
